38bdf3cffbd6efb9a21ca94d2acde1f63eed0e84
tomato2718 <qweasdzxc030@gmail.com>
2023-11-26 19:28:56 +0800

    Tests: test Reader class

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,24 @@
+from typing import Any, TypeAlias
+
 import pytest
+from pymongo import MongoClient
+from pymongo.database import Database
+
+T: TypeAlias = dict[str, Any]
+
+@pytest.fixture(scope="session")
+def client() -> MongoClient[T]:
+    client = MongoClient(host="0.0.0.0", port=8017, document_class=T)
+    return client
+
+
+@pytest.fixture(scope="session")
+def database(client: MongoClient[T]) -> Database[T]:
+    DATABASE_NAME = "testing_database"
+    database = client.get_database(DATABASE_NAME)
+    return database
+
+@pytest.fixture(scope="session")
+def collection() -> str:
+    COLLECTION_NAME = "testing_collection"
+    return COLLECTION_NAME
