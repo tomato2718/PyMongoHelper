4c66dbe1372f6d27f8063c922001d70c6f9a9cfa
tomato2718 <qweasdzxc030@gmail.com>
2024-02-26 14:25:49 +0800

    Chore: 0.4.0

--- pymongohelper/__about__.py
+++ pymongohelper/__about__.py
@@ -12,8 +12,8 @@ __all__ = [
 ]
 
 __project__ = "pymongohelper"
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 __author__ = "yveschen2718@gmail.com"
 __maintainer__ = "yveschen2718@gmail.com"
-__release__ = "2024/01/23"
+__release__ = "2024/02/26"
 __summary__ = "A library help you design pymongo client more clean."
